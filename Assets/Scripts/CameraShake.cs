3763a5b9fc834a965ffc172243fab31768584699
Max Schlosser <mschlos3@hs-mittweida.de>
2022-06-28 17:56:09 +0200

    Add cam shake

---
+++ Assets/Scripts/CameraShake.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+
+public class CameraShake : MonoBehaviour
+{
+    private Animator animator;
+    private static readonly int Shake = Animator.StringToHash("Shake");
+
+    private void Start()
+    {
+        animator = GetComponent<Animator>();
+        FindObjectOfType<PointManager>().OnScoreUpdate += HandleScoreUpdate;
+    }
+
+    private void HandleScoreUpdate(PaddleMovement paddleMovement, int score)
+    {
+        animator.SetTrigger(Shake);
+    }
+}
